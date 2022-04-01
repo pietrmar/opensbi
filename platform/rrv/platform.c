f21e74ffffdaaed6ea6858668dd686db9d3cd860
Martin Pietryka <martin@pietryka.at>
2022-04-01 04:22:58 +0200

    platform: rrv: add aclint timer and cleanup code a bit
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- platform/rrv/platform.c
+++ platform/rrv/platform.c
@@ -4,22 +4,34 @@
  * Copyright (c) 2019 Western Digital Corporation or its affiliates.
  */
 
+#include <sbi/riscv_io.h>
 #include <sbi/riscv_asm.h>
 #include <sbi/riscv_encoding.h>
 #include <sbi/sbi_const.h>
 #include <sbi/sbi_platform.h>
 #include <sbi/sbi_console.h>
+#include <sbi_utils/timer/aclint_mtimer.h>
 
-#include "regs.h"
+#include "rrv_regs.h"
 
 static void rrv_debug_putc(char ch)
 {
-	REG_WRITE(DEBUG_OUTPUT_REG, ch);
+	writel(ch, (uint32_t *)RRV_DEBUG_OUTPUT_REG);
 }
 
 static struct sbi_console_device rrv_debug_console = {
 	.name = "rrv-debug",
 	.console_putc = rrv_debug_putc,
+};
+
+static struct aclint_mtimer_data mtimer = {
+	.mtime_freq = RRV_ACLINT_MTIME_FREQ,
+	.mtime_addr = RRV_ACLINT_MTIME_ADDR,
+	.mtime_size = ACLINT_DEFAULT_MTIME_SIZE,
+	.mtimecmp_addr = RRV_ACLINT_MTIMECMP_ADDR,
+	.mtimecmp_size = RRV_ACLINT_MTIMECMP_SIZE,
+	.first_hartid = 0,
+	.hart_count = 1,
 };
 
 /*
@@ -31,11 +43,25 @@ static int platform_console_init(void)
 	return 0;
 }
 
+static int platform_timer_init(bool cold_boot)
+{
+	int rc;
+
+	if (cold_boot) {
+		rc = aclint_mtimer_cold_init(&mtimer, NULL);
+		if (rc)
+			return rc;
+	}
+
+	return aclint_mtimer_warm_init();
+}
+
 /*
  * Platform descriptor.
  */
 const struct sbi_platform_operations platform_ops = {
 	.console_init		= platform_console_init,
+	.timer_init		= platform_timer_init,
 };
 
 const struct sbi_platform platform = {
--- platform/rrv/regs.h
+++
@@ -1,9 +0,0 @@
-#ifndef __REGS__H__
-#define __REGS__H__
-
-#define REG_READ(__reg) (*(volatile uint32_t *)(__reg))
-#define REG_WRITE(__reg, __value) (*(volatile uint32_t *)(__reg) = (__value))
-
-#define DEBUG_OUTPUT_REG	0x40001000
-
-#endif /* __REGS__H__ */
