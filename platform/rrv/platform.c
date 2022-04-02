1a75c01f596162657ce46dd02095b2c053042436
Martin Pietryka <martin@pietryka.at>
2022-04-02 16:13:30 +0200

    platform: rrv: add aclint mswi and clean up the code a bit
    Signed-off-by: Martin Pietryka <martin@pietryka.at>

--- platform/rrv/platform.c
+++ platform/rrv/platform.c
@@ -11,6 +11,7 @@
 #include <sbi/sbi_platform.h>
 #include <sbi/sbi_console.h>
 #include <sbi_utils/timer/aclint_mtimer.h>
+#include <sbi_utils/ipi/aclint_mswi.h>
 
 #include "rrv_regs.h"
 
@@ -24,14 +25,26 @@ static struct sbi_console_device rrv_debug_console = {
 	.console_putc = rrv_debug_putc,
 };
 
-static struct aclint_mtimer_data mtimer = {
-	.mtime_freq = RRV_ACLINT_MTIME_FREQ,
-	.mtime_addr = RRV_ACLINT_MTIME_ADDR,
-	.mtime_size = ACLINT_DEFAULT_MTIME_SIZE,
-	.mtimecmp_addr = RRV_ACLINT_MTIMECMP_ADDR,
-	.mtimecmp_size = RRV_ACLINT_MTIMECMP_SIZE,
+static struct aclint_mswi_data mswi = {
+	.addr = RRV_ACLINT_BASE +
+		CLINT_MSWI_OFFSET,
+	.size = ACLINT_MSWI_SIZE,
 	.first_hartid = 0,
-	.hart_count = 1,
+	.hart_count = RRV_HART_COUNT,
+};
+
+static struct aclint_mtimer_data mtimer = {
+	.mtime_freq = RRV_ACLINT_MTIMER_FREQ,
+	.mtime_addr = RRV_ACLINT_BASE +
+		      CLINT_MTIMER_OFFSET +
+		      ACLINT_DEFAULT_MTIME_OFFSET,
+	.mtime_size = ACLINT_DEFAULT_MTIME_SIZE,
+	.mtimecmp_addr = RRV_ACLINT_BASE +
+			 CLINT_MTIMER_OFFSET +
+			 ACLINT_DEFAULT_MTIMECMP_OFFSET,
+	.mtimecmp_size = ACLINT_DEFAULT_MTIMECMP_SIZE,
+	.first_hartid = 0,
+	.hart_count = RRV_HART_COUNT,
 };
 
 /*
@@ -56,12 +69,26 @@ static int platform_timer_init(bool cold_boot)
 	return aclint_mtimer_warm_init();
 }
 
+static int platform_ipi_init(bool cold_boot)
+{
+	int rc;
+
+	if (cold_boot) {
+		rc = aclint_mswi_cold_init(&mswi);
+		if (rc)
+			return rc;
+	}
+
+	return aclint_mswi_warm_init();
+}
+
 /*
  * Platform descriptor.
  */
 const struct sbi_platform_operations platform_ops = {
 	.console_init		= platform_console_init,
 	.timer_init		= platform_timer_init,
+	.ipi_init		= platform_ipi_init,
 };
 
 const struct sbi_platform platform = {
